683d1ac89fbab60d20b7d82ab289bd4a32547172
Johnny Jurak <johny9020@hotmail.com>
2024-01-18 11:21:04 +0100

    change: Changed design for the website

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,62 @@
-import {Button} from "./Button.tsx"
-import logo from '../assets/GSJ-Logo.png'
+import logo from '../assets/GSJ-Logo-White.png'
 import {Menu} from "lucide-react";
-import {useState} from "react";
+import {Button} from "./Button.tsx";
 
 export function Navbar() {
-    const [showingMenu, setShowMenu] = useState(false)
 
     return (
-        <div className='flex flex-col'>
-            <div className='flex justify-between items-center p-2.5 mb-5 mt-5 lg:mb-2 lg:mt-2'>
-                <div className='flex-shrink-0 h-auto w-[200px] md:w-[250px] lg:w-[300px]'>
-                    <img src={logo} alt='Testing'/>
+        <div className={`flex flex-col header`}>
+            {/* Navbar */}
+            <div className='flex justify-between p-2.5'>
+                <div className='flex flex-shrink-0 w-[200px] h-auto'>
+                    <img src={logo} alt='loading'/>
                 </div>
 
-                <Button onClick={() => {
-                    setShowMenu(!showingMenu)
-                }} variant='ghost' size='icon' className='sm:hidden'>
-                    <Menu/>
-                </Button>
+                <div className='hidden md:flex gap-2.5 flex-shrink-0 items-center'>
+                    <Button variant='link'>
+                        Home
+                    </Button>
 
-                <div className='hidden sm:flex'>
-                    <Button variant='link'>Home</Button>
-                    <Button variant='link'>Kontakt</Button>
-                    <Button variant='link'>Fotogalerie</Button>
+                    <Button variant='link'>
+                        Kontakt
+                    </Button>
+
+                    <Button variant='link'>
+                        Fotogalerie
+                    </Button>
+                </div>
+
+                <div className='hidden md:flex flex-shrink-0 items-center'>
+                    <Button>
+                        Get started
+                    </Button>
+                </div>
+
+                <div className='md:hidden'>
+                    <Button variant='ghost' size='icon'>
+                        <Menu/>
+                    </Button>
                 </div>
             </div>
 
-            {showingMenu && (
-                <div
-                    className='sm:hidden flex flex-col items-center gap-1.5 bg-gray-400 text-white p-2.5'>
-                    <Button variant='link_bg' size='link' className='w-full'>Home</Button>
-                    <Button variant='link_bg' size='link' className='w-full'>Kontakt</Button>
-                    <Button variant='link_bg' size='link' className='w-full'>Fotogalerie</Button>
-                </div>
-            )}
+            <div className='flex flex-col justify-center items-center p-2 gap-5 lg:gap-3 text-center mt-auto mb-auto'>
+                <h1 className='font-bold text-2xl lg:text-3xl'>
+                    Profesionální společnost, která se specializuje v oboru Instalatérství, Topenářství, Sváření a
+                    Plynu
+                </h1>
+
+                <h2 className='font-bold text-xl lg:text-2xl'>
+                    Naší další specializací jsou stavební práce, výkopové práce a bourací
+                    práce.
+                </h2>
+
+                <p className='text-balance text-sm'>Naší specializací jsou svářečské práce a kompletní realizace
+                    vodovodního
+                    řádu, kanalizace, topení a
+                    plynu. Rozsah prací realizujeme v Ústeckém kraji, Středočeském kraji a v Praze, zejména však Most,
+                    Chomutov, Jirkov, Kadaň, Louny, Žatec a okolí.
+                </p>
+            </div>
         </div>
     )
 }
